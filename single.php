60aa64420847a16715b4f011e647230cc7d38fc2
Kainat <anmolkainat244@gmail.com>
2023-04-07 02:19:00 -0700

    Addes Custom Fields

--- single.php
+++ single.php
@@ -24,29 +24,32 @@
   <thead>
     <tr>
       <th scope="col"><?php the_field('title', get_the_id());?></th>
-      <th scope="col">PKR 20.0 - 48.8 <br>lacs lacs</th>
-      <th scope="col">Body Type</th>
-      <th scope="col">Sedan</th>
+      <th scope="col"><?php the_field('price', get_the_id());?></th>
+      <th scope="col">Fuel Type</th>
+      <th scope="col"><?php the_field('fuel_type', get_the_id());?></th>
     </tr>
   </thead>
   <tbody>
     <tr>
       <th scope="row">Dimensions</th>
-      <td>4620 x 1775 x 1475 mm</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+      <td><?php the_field('dimensions', get_the_id());?></td>
+      <td>Fuel Tank Capacity</td>
+      <td><?php the_field('fuel_tank_capacity', get_the_id());?></td>
     </tr>
     <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
+      <th scope="row">Displacement</th>
+      <td><?php the_field('displacement', get_the_id());?></td>
+      <td>Top Speed</td>
+      <td><?php the_field('top_speed', get_the_id());?></td>
     </tr>
+
     <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
+      <th scope="row">Horse Power</th>
+      <td><?php the_field('horse_power', get_the_id());?></td>
+      <td>Seating Capacity</td>
+      <td><?php the_field('seating_capacity', get_the_id());?></td>
     </tr>
+    
   </tbody>
 </table>
 
